0b7fc21f2529310ce40a0d12da0c8dc8b11eeb10
biguncle <biguncle@192.168.5.137>
2026-03-18 22:29:33 +0800

    init

--- secnano/backends/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Execution backends."""
-
-from secnano.backends.base import SubagentBackend
-from secnano.backends.host import HostBackend
-from secnano.backends.pyclaw_container import PyclawContainerBackend
-from secnano.backends.subprocess_backend import SubprocessBackend
-
-__all__ = ["SubagentBackend", "HostBackend", "PyclawContainerBackend", "SubprocessBackend"]
--- secnano/process/__init__.py
+++
@@ -1,6 +0,0 @@
-"""子进程执行模块。"""
-
-from secnano.process.protocol import WorkerInput, WorkerMessage
-from secnano.process.pool import ProcessPool
-
-__all__ = ["ProcessPool", "WorkerInput", "WorkerMessage"]
